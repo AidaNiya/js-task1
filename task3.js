ff522cc7dda3bf733e1806245a8a520a0884a54f
Aida Niyazbekova <aida.niyazbekova.on@gmail.com>
2022-01-11 17:14:56 +0600

    finish 3rd task

--- task3.js
+++ task3.js
@@ -1,7 +1,21 @@
-/*let userName = "Aida Niyazbekova";
+/* Вывод данных один за другим
+let userName = "Aida Niyazbekova";
 let userMail = "aida.niyazbekova.on@gmail.com";
-alert("Имя пользователя: " + userInfo);
+alert("Имя пользователя: " + userName);
 alert("Почта пользователя: " + userMail);
- */
-let userName = promt("Username-Aida Niyazbekova");
-alert(userName);
+*/
+//
+let userName = "Aida Niyazbekova";
+let userMail = "aida.niyazbekova.on@gmail.com";
+alert(
+  `Имя пользователя: ` +
+    userName +
+    `
+Почта пользователя: ` +
+    userMail
+);
+
+/* простой вывод данных
+alert(`Имя вользователя: Aida niyazbekova
+Почта пользователя: aida.niyazbekova.on.gmail.com`);
+*/
